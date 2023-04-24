103b5f81d59a40094f0c54f6409c7ec2ca2c0bc9
Marcos Winther Silva Carreiro <44624583+MarcosWinther@users.noreply.github.com>
2023-04-24 13:06:22 -0300

    Testando nova implementação no arquivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
+    document.body.classList.remove('w3-light-grey');
     document.body.classList.toggle('w3-dark-grey');
 })
